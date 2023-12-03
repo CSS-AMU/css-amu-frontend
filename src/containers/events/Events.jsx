347693f73d7a2f0b7b266982055565b6794db697
Admin CSS AMU <47985750+css-amu-admin@users.noreply.github.com>
2023-12-03 19:09:45 +0530

    Update Events.jsx
    updating events list

--- src/containers/events/Events.jsx
+++ src/containers/events/Events.jsx
@@ -10,7 +10,11 @@ const Events = () => (
     <div className="csd__events-content">
       <h4>EVENTS CONDUCTED BY COMPUTER SCIENCE SOCIETY</h4>
       <h1 className="gradient__text">We organized various events <br /> gaining participants from throughout the COUNTRY.</h1>
-       <p>Winter of code January, 2022<br />
+       <p>CodeHawk Coding Competition, September 20, 2023<br/>
+          Web it up !  A one-week workshop on Web Development, September 19, 2022<br />
+          Poster & Logo Making Competition (Theme Ragging Free Campus), August 16,2023<br />
+          Speech Competition on the topic "Role of ICT in Nation Building, August 10, 2022<br />
+          Winter of code January, 2022<br />
           Career Paths in IT Industry: Present and Future December 23, 2021<br />
           Android Study Jams December, 2021<br />
           Build and Deploy your first website (Hands-on workshop) October  31, 2021<br />
